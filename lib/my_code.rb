b42de914cd4d7ed7e681648f795417fb46a726cd
rtfp0102 <rtfp0102@gmail.com>
2019-10-13 18:36:00 +0000

    Done.

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -1,1 +1,33 @@
-# Your Code Here
+def map(array)
+  new_array = []
+  i = 0 
+  while i < array.length do
+    n = array[i]
+    new_array.push(yield(n))
+    i += 1 
+  end
+  return new_array
+end
+map([1, 2, 3, -9]) {|n| n * -1}
+
+def reduce(source_array, start = nil)
+  if start 
+    memo = start
+    i = 0 
+    
+  else 
+    memo = source_array[0] 
+    i = 1 
+  end
+  while  i < source_array.length do
+    n = source_array[i]
+    memo = yield(memo, n)
+    i += 1 
+  end
+  return memo
+end
+
+
+
+
+
